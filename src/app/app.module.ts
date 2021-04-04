c252ade4451c26c78eac792f576d6b24f67b86e2
Zahid Hassan <zahid13@cse.pstu.ac.bd>
2021-04-04 17:32:37 +0600

    component folder added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        CoursesComponent
     ],
     imports: [
         BrowserModule,
