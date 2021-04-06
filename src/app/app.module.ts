f41f1030dd42ff13570f855011911a2d316e1446
Zahid Hassan <zahid13@cse.pstu.ac.bd>
2021-04-07 00:51:48 +0600

    course-component bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { CoursesComponent } from './courses/courses.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        CoursesComponent
+        CoursesComponent,
+        ContactFormComponent
     ],
     imports: [
         BrowserModule,
